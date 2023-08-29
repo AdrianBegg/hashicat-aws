22d61d77e39ba1d50a4b9378d6ba9921ec17a02d
Adrian Begg <adriabeg@amazon.com>
2023-08-29 15:45:23 +0000

    Added the first tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "adrianbegg"
     workspaces {
       name = "hashicat-aws"
